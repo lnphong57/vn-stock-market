8f57de0ef2cbd8e191d57a046076d448d5137ec2
Lnphong57 <lnam.phong57@gmail.com>
2026-01-29 07:16:18 +0700

    add url for each symbol

--- src/const.py
+++ src/const.py
@@ -4,3 +4,26 @@ bankId = [
         ]
 marketIndex = ["VNINDEX"]
 allSymbols = bankId + marketIndex
+url = [       
+        "https://finance.vietstock.vn/ACB-ngan-hang-tmcp-a-chau.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/BID-ngan-hang-tmcp-dau-tu-va-phat-trien-viet-nam.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/CTG-ngan-hang-tmcp-cong-thuong-viet-nam.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/EIB-ngan-hang-tmcp-xuat-nhap-khau-viet-nam.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/EVF-cong-ty-tai-chinh-tong-hop-co-phan-dien-luc.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/HDB-ngan-hang-tmcp-phat-trien-thanh-pho-ho-chi-minh.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/KLB-ngan-hang-tmcp-kien-long.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/LPB-ngan-hang-tmcp-loc-phat-viet-nam.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/MBB-ngan-hang-tmcp-quan-doi.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/MSB-ngan-hang-tmcp-hang-hai-viet-nam.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/NAB-ngan-hang-tmcp-nam-a.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/OCB-ngan-hang-tmcp-phuong-dong.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/PVF-tong-cong-ty-tai-chinh-co-phan-dau-khi-viet-nam.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/SHB-ngan-hang-tmcp-sai-gon-ha-noi.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/SSB-ngan-hang-tmcp-dong-nam-a.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/STB-ngan-hang-tmcp-sai-gon-thuong-tin.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/TCB-ngan-hang-tmcp-ky-thuong-viet-nam.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/TPB-ngan-hang-tmcp-tien-phong.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/VAB-ngan-hang-tmcp-viet-a.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/VIB-ngan-hang-tmcp-quoc-te-viet-nam.htm?tab=BCTQ",
+        "https://finance.vietstock.vn/VPB-ngan-hang-tmcp-viet-nam-thinh-vuong.htm?tab=BCTQ"
+        ]
