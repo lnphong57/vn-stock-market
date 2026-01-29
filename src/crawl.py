4c1704d44874c500567da8cc0a0a004fcfbf32b5
Lnphong57 <lnam.phong57@gmail.com>
2026-01-29 07:14:48 +0700

    crawl bctc

--- src/crawl.py
+++ src/crawl.py
@@ -1,19 +1,43 @@
 import requests
+import json
+from playwright.sync_api import sync_playwright
 class Crawler:  
-    def __init__(self, banks, marketIndex):
+    def __init__(self, banks, marketIndex, url):
         self.banks = banks
         self.marketIndex = marketIndex
+        self.url = url
         self.allSymbols = banks + marketIndex
-    def crawl_info(self):
-        url = "https://cafef.vn/du-lieu/Ajax/PageNew/ChiSoTaiChinh.ashx?Symbol=" 
-        tempData = []
-        for id in self.banks:
-            fullUrl = url + id
-            response = requests.get(fullUrl)
-            data = response.json()
-            data["ticker"] = id.upper()
-            tempData.append(data)
-        return tempData
+    def crawl_bctc(self):
+        all_data = {}
+        with sync_playwright() as p:
+            browser = p.chromium.launch(headless=True, args=["--disable-blink-features=AutomationControlled", "--start-maximized"])
+
+        # Tạo context với User Agent thật (giả làm Chrome trên Windows 10)
+            context = browser.new_context(
+                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                viewport={"width": 1920, "height": 1080} # Set màn hình to để tránh giao diện mobile
+            )
+            for link, symbol in zip(const.url, const.bankId):
+                temp = []
+                page = context.new_page()
+                page.goto(link, timeout=60000)
+                finance = page.locator("div.pos-relative")
+                tables = finance.locator("div.table-responsive")
+                firstTable = tables.nth(0)
+                head = firstTable.locator("thead tr th.text-center.col-100.al-middle b")
+                temp.append(head.all_inner_texts())
+                for i in range(3):
+                    table = tables.nth(i)              
+                    rows = table.locator("tbody tr")
+                    for x in range(rows.count()):
+                        row = rows.nth(x)
+                        cells = row.locator("td")
+                        row_data = cells.all_inner_texts()
+                        temp.append(row_data)
+                
+                all_data[symbol] = temp
+            browser.close()
+        return all_data
     
     def crawl_price_history(self):
         symbolUrl = "https://cafef.vn/du-lieu/Ajax/PageNew/DataHistory/PriceHistory.ashx?Symbol="
@@ -28,10 +52,5 @@ class Crawler:
                 data["ticker"] = id.upper()
                 tempData.append(data)
         return tempData
-            
-
-        
-
-
-
-
+    
+    
